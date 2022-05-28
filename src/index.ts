a17f2ba7f580911c078340f6c98d5ca21e77eced
JMBenitez-dev <105891765+JMBenitez-dev@users.noreply.github.com>
2022-05-28 01:44:32 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 //declare the first array
+//la correción la hicimos en clase pero para formalizar aca va:
 let v1: number[] = new Array(6);
 let numberv1: number;
-let indicev1: number;
+let indicev1: number;// que pasa si usas un solo indice en vez de uno por cada for?
 //declaro el prompt de ingreso de valores
-for (indicev1 = 0; indicev1 < 6; indicev1++) {
+for (indicev1 = 0; indicev1 < 6; indicev1++) {// lo ideal seria usar v1.length ya que si necesitas cambiar la cantidad de elementos solo cambias en la primer asignacion y no en el resto del codigo.
   numberv1 = Number(
     prompt(
       "Indique el número que desea incorporar en la posición " + indicev1,
@@ -40,13 +41,13 @@ for (indicev2 = 0; indicev2 < 6; indicev2++) {
 let vSuma: number[] = new Array(6);
 let indicevSuma: number;
 for (indicevSuma = 0; indicevSuma < 6; indicevSuma++) {
-  vSuma[indicevSuma] = v1[indicev1] + v2[indicev2];
+  vSuma[indicevSuma] = v1[indicev1] + v2[indicev2];//ojo con el indice que usas, indicev1 y indicev2 son correctos? o que deberias usar?
 }
 let i: number = 0;
 for (i = 0; i < 6; i++) {
   console.log("el resultado de " + vSuma[indicevSuma]);
 }
-
+//solucionado lo de los indices la suma que esta arriba va a funcionar y la de abajo deja de tener sentido
 console.log(
   "la suma de los numeros igresados es :",
   v1[0] + v2[0],
